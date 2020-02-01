49521d02594b16e8e08e2e2fd4672edb67632100
Jason Bell <jbell69@mac.com>
2020-02-01 14:50:06 -0600

    v2

--- UFO-level-2/static/js/reportingTable.js
+++ UFO-level-2/static/js/reportingTable.js
@@ -24,31 +24,40 @@ var button = d3.select("#filter-btn");
 
 button.on("click", function () {
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  // // Select the input element and get the raw HTML node
+  // var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  // // Get the value property of the input element
+  // var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  //console.log(UFOsightingData);
+  // console.log(inputValue);
+  // //console.log(UFOsightingData);
 
-  var filteredSightingData = UFOsightingData.filter(UFOsightingData => UFOsightingData.datetime === inputValue);
+  // var filteredSightingData = UFOsightingData.filter(UFOsightingData => UFOsightingData.datetime === inputValue);
 
-  console.log(filteredSightingData);
+  // console.log(filteredSightingData);
 
-  // // Material Select Initialization
-  // $(document).ready(function () {
-  //   $('.mdb-select').materialSelect();
-  // });
 
+  function filterTable(filters) {
+    // Set the filteredData to the tableData
+    let filteredData = UFOsightingData;
+    // Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+    // Finally, rebuild the table using the filtered Data
+    buildTable(filteredData);
+  }
+  // Attach an event to listen for changes to each filter
+  d3.selectAll(".filter").on("change", updateFilters);
 
   tbody.html("");
 
-  filteredSightingData.forEach(function (UpdateTable) {
-    console.log(UpdateTable);
+  filteredSightingData.forEach(function (buildTable) {
+    console.log(buildTable);
     var row = tbody.append("tr");
-    Object.entries(UpdateTable).forEach(function ([key, value]) {
+    Object.entries(buildTable).forEach(function ([key, value]) {
       console.log(key, value);
       // Append a cell to the row for each value
       // in the weather report object
@@ -56,5 +65,4 @@ button.on("click", function () {
       cell.text(value);
     });
   });
-
 });
